c7f4ab042dc77cf4149a287a09806cba01261608
Melbin Mathew <melbin1997@gmail.com>
2016-10-16 18:49:00 +0530

    Gather price of a car

---
+++ price.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Firefox()
+driver.get("https://www.cardekho.com/overview/Jeep_Grand_Cherokee/Jeep_Grand_Cherokee_Summit_4X4.htm")
+
+price = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class = 'priceleft']//span")))
+print price[2].text,price[3].text[:-1]
+
+driver.close()
