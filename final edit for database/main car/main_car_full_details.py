c95e0e256589d1cfc7df8efa53640d09673093cb
georgytheboss <georgyjose007@gmail.com>
2016-10-19 00:53:46 +0530

    Main car complete data for database

--- final edit for database/main car/main_car_full_details.py
+++ final edit for database/main car/main_car_full_details.py
@@ -12,30 +12,22 @@ for i in data.keys():
 	for j in data[i]:
 		try:
 			main={}
-			#print ""
-			#print "Car Name: ",j
 			main['Car Name']=j
-			#print ""
-			#print "Car Price: ",int(data[i][j]['Key Specs'][0]['Ex-showroom Price New Delhi'][2:-15].replace(",",""))
 			main['Car Price']=int(data[i][j]['Key Specs'][0]['Ex-showroom Price New Delhi'][2:-15].replace(",",""))
 			for k in image.keys():
 				if (k==j):
-					#print ""
-					#print "Car images: "
-					#print image[j]
 					main['Car Images']=image[j]
 					break
-			#print ""
-			#print "Car Mileage: ",sum(map(float,data[i][j]['Key Specs'][0]['City / Highway Mileage'].replace(" kmpl","").split(" / ")))/float(len(data[i][j]['Key Specs'][0]['City / Highway Mileage'].replace(" kmpl","").split(" / ")))
 			if(j=='Mahindra e2o'):
 				main['Car Mileage']='120 km on full charge'	
 			else:
 				main['Car Mileage']=sum(map(float,data[i][j]['Key Specs'][0]['City / Highway Mileage'].replace(" kmpl","").split(" / ")))/float(len(data[i][j]['Key Specs'][0]['City / Highway Mileage'].replace(" kmpl","").split(" / ")))
-			#print ""
-			#print data[i][j]
 			main['Car Features']=data[i][j]
 			main['Car Type']=car_type[j]
-			main['Car Review']=review[j]
+			if j in ['Audi RS7', 'Ford Figo', 'Porsche Cayenne', 'Rolls-Royce Ghost', 'Maserati Ghibli', 'Nissan Terrano', 'Mercedes-Benz AMG GT', 'BMW 6 Series', 'Bugatti Veyron', 'Ford Mustang', 'Jaguar XJ', 'Mahindra Verito']:
+				main['Car Review']="Sorry no data available now."
+			else:
+				main['Car Review']=review[j]
 		
 
 
@@ -47,4 +39,3 @@ for i in data.keys():
 print ""
 print ""
 print err
-#print main
